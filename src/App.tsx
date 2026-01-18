dd7a67f2199088dffbaf3d912e95bbc0224f0e78
aravsahni7 <aravsahni7@gmail.com>
2026-01-17 19:23:51 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/Cerasp-Demo">
       <Chatbot />
         <Routes>
           <Route path="/" element={<Index />} />
